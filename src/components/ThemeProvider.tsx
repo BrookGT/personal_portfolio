ad21f6640ec54a287d2162523515f138d336b530
Biruk GT <birukgebisa7@gmail.com>
2025-05-23 15:47:05 +0300

    fixed toggle theme

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -16,11 +16,13 @@ type ThemeProviderProps = {
 type ThemeProviderState = {
     theme: Theme;
     setTheme: (theme: Theme) => void;
+    toggleTheme: () => void;
 };
 
 const initialState: ThemeProviderState = {
     theme: "system",
     setTheme: () => null,
+    toggleTheme: () => null,
 };
 
 const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
@@ -62,6 +64,11 @@ export function ThemeProvider({
         setTheme: (theme: Theme) => {
             localStorage.setItem(storageKey, theme);
             setTheme(theme);
+        },
+        toggleTheme: () => {
+            const newTheme = theme === "light" ? "dark" : "light";
+            localStorage.setItem(storageKey, newTheme);
+            setTheme(newTheme);
         },
     };
 
